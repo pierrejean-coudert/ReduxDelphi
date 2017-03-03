8700a70ab3a8f93497185148ec81f1b4541a1a31
Pierre-Jean Coudert <pierrejean.coudert@gmail.com>
2017-03-03 10:31:52 +0100

    Store: add middleware support

--- Src/ReduxD/Redux.pas
+++ Src/ReduxD/Redux.pas
@@ -8,37 +8,66 @@ uses
 
 type
 
-//TODO : add middlewares
-//TODO : add TODO sample
+//TODo : add tests
+//TODO : add TODOList sample
 //TODO : add async sample
 
   TSubscriber<TState> = reference to procedure (State: TState);
 
   TReducer<TState, TAction> = reference to function (State: TState; Action: TAction): TState;
 
-//  TDispatcher<TAction> = reference to function(Action: TAction): TAction;
-//  TStore<TState, TAction> = class;
-  TMiddleware<TAction> = reference to function(Action: TAction) : TAction;
+  TDispatcher<TAction> = reference to function(Action: TAction): TAction;
+  TMiddleware<TAction> = reference to function(Dispatcher: TDispatcher<TAction>) : TDispatcher<TAction>;
 
-  TStore<TState, TAction> = class
+  IStore<TState, TAction> = interface
+    procedure Subscribe(Subscriber : TSubscriber<TState>);
+    procedure Dispatch(Action: TAction);
+    procedure AddMiddleware(Middleware : TMiddleware<TAction>);
+    function  GetState(): TState;
+  end;
+
+  TStore<TState, TAction> = class(TInterfacedObject,IStore<TState, TAction>)
   private
     FState : TState;
     FReducer : TReducer<TState, TAction>;
     FSubscriber : TList<TSubscriber<TState>>;
     FMiddlewares : TList<TMiddleware<TAction>>;
 
-  public
-      constructor Create(Reducer: TReducer<TState, TAction>; State:TState);
+    function innerDispatch(Action: TAction): TAction;
+    function applyMiddleware(): TDispatcher<TAction>;
 
-      procedure Subscribe(Subscriber : TSubscriber<TState>);
-      procedure Dispatch(Action: TAction);
-      procedure applyMiddleware(Middleware : TMiddleware<TAction>);
-      function  GetState(): TState;
+  public
+    constructor Create(Reducer: TReducer<TState, TAction>; State:TState);
+
+    procedure Subscribe(Subscriber : TSubscriber<TState>);
+    procedure Dispatch(Action: TAction);
+    procedure AddMiddleware(Middleware : TMiddleware<TAction>);
+    function  GetState(): TState;
   end;
 
 implementation
 
 { TStore<TState, TAction> }
+
+function TStore<TState, TAction>.applyMiddleware: TDispatcher<TAction>;
+var
+  dispatcher : TDispatcher<TAction>;
+  middleware : TMiddleware<TAction>;
+begin
+  dispatcher := innerDispatch;
+ 	for middleware in FMiddlewares do
+		dispatcher := middleware(dispatcher);
+  result := dispatcher;
+end;
+
+function TStore<TState, TAction>.innerDispatch(Action: TAction): TAction;
+var
+  Subscriber : TSubscriber<TState>;
+begin
+  FState := FReducer(FState, Action);
+ 	for Subscriber in FSubscriber do
+		Subscriber(FState);
+end;
 
 constructor TStore<TState, TAction>.Create(Reducer: TReducer<TState, TAction>;
                                            State: TState);
@@ -49,18 +78,14 @@ begin
   FMiddlewares := TList<TMiddleware<TAction>>.Create();
 end;
 
-procedure TStore<TState, TAction>.applyMiddleware(Middleware: TMiddleware<TAction>);
+procedure TStore<TState, TAction>.AddMiddleware(Middleware: TMiddleware<TAction>);
 begin
   FMiddlewares.Add(Middleware);
 end;
 
 procedure TStore<TState, TAction>.Dispatch(Action: TAction);
-var
-  Subscriber : TSubscriber<TState>;
 begin
-  FState := FReducer(FState, Action);
- 	for Subscriber in FSubscriber do
-		Subscriber(FState);
+  applyMiddleware()(Action);
 end;
 
 function TStore<TState, TAction>.GetState: TState;
